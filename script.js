68178b82c4088105ffb46522ec33a21a5d636729
VINETTE KARINE <71769675+KarineVinette@users.noreply.github.com>
2023-07-18 07:45:35 +0200

    add colors

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const colors = [
-    { group: "Reds", shades: [{ name: "Crimson", code: "#DC143C" }, { name: "Tomato", code: "#FF6347" }, { name: "IndianRed", code: "#CD5C5C" }, { name: "FireBrick", code: "#B22222" }] },
-    { group: "Greens", shades: [{ name: "LimeGreen", code: "#32CD32" }, { name: "ForestGreen", code: "#228B22" }, { name: "OliveDrab", code: "#6B8E23" }, { name: "DarkOliveGreen", code: "#556B2F" }] },
+    { group: "1", shades: [{ name: "Crimson", code: "#117C6F" }, { name: "Tomato", code: "#289C8E" }, { name: "IndianRed", code: "#2FC4B2" }, { name: "FireBrick", code: "#8DE5DB" }, { name: "FireBrick", code: "#B2F0E8" } ] },
+    { group: "2", shades: [{ name: "LimeGreen", code: "#4A707A" }, { name: "ForestGreen", code: "#7697A0" }, { name: "OliveDrab", code: "#94B0B7" }, { name: "DarkOliveGreen", code: "#C2C8C5" },{ name: "DarkOliveGreen", code: "#DDDDDA" } ] },
     // Add more color groups and their shades as needed
   ];
   
@@ -16,11 +16,20 @@ const colors = [
     colorRow.appendChild(colorName);
   
     group.shades.forEach((color) => {
+      const colorContainer = document.createElement("div");
+      colorContainer.classList.add("color-container");
+  
       const colorBox = document.createElement("div");
       colorBox.classList.add("color-box");
       colorBox.style.backgroundColor = color.code;
+      colorContainer.appendChild(colorBox);
   
-      colorRow.appendChild(colorBox);
+      const colorCode = document.createElement("div");
+      colorCode.classList.add("color-code");
+      colorCode.textContent = color.code;
+      colorContainer.appendChild(colorCode);
+  
+      colorRow.appendChild(colorContainer);
     });
   
     colorPalette.appendChild(colorRow);
