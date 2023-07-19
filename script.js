db8ef9c8920b7429bd8c9e859201f58c9f708728
VINETTE KARINE <71769675+KarineVinette@users.noreply.github.com>
2023-07-19 04:19:45 +0200

    add colors

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const colors = [
     { group: "1", shades: [{ code: "#117C6F" }, { code: "#289C8E" }, { code: "#2FC4B2" }, { code: "#8DE5DB" }, { code: "#B2F0E8" } ] },
     { group: "2", shades: [{ code: "#4A707A" }, { code: "#7697A0" }, { code: "#94B0B7" }, { code: "#C2C8C5" },{  code: "#DDDDDA" } ] },
     // Add more color groups and their shades as needed
+    { group: "3", shades: [{ code: "#2C121B" }, { code: "#F26F14" }, { code: "#FBD3B7" }, { code: "#EEE7DF" } ] },
+    { group: "4", shades: [{ code: "#0C133B" }, { code: "#32D3E7" }, { code: "#EBF4F1" }, { code: "#D1E0DB" } ] },
+   
   ];
   
   const colorPalette = document.querySelector(".color-palette");
